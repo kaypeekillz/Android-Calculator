244de7685c647f9bd698da07ef220dbcf459c487
kehinde peter <kehinde.onyekuwuluje@xputertechnologies.com>
2019-12-18 11:08:02 +0100

    First commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
+
+class MainActivity: AppCompatActivity() {
+
+//    displayValue: '0',
+//    firstOperand: null,
+//    waitingForSecondOperand: false,
+//    operator: null,
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var displayValue = "0"
+//        var firstOperand = null
+//        var waitingForSecondOperand = false
+//        var operator = null
+//        var clear = true
+
+        dotBtn.setOnClickListener {
+            solveProblem(".", true)
+        }
+        zeroBtn.setOnClickListener {
+            solveProblem("0", true)
+        }
+        oneBtn.setOnClickListener {
+            solveProblem("1", true)
+        }
+        twoBtn.setOnClickListener {
+            solveProblem("2", true)
+        }
+        threeBtn.setOnClickListener {
+            solveProblem("3", true)
+        }
+        fourBtn.setOnClickListener {
+            solveProblem("4", true)
+        }
+        fiveBtn.setOnClickListener {
+            solveProblem("5", true)
+        }
+        sixBtn.setOnClickListener {
+            solveProblem("6", true)
+        }
+        sevenBtn.setOnClickListener {
+            solveProblem("7", true)
+        }
+        eightBtn.setOnClickListener {
+            solveProblem("8", true)
+        }
+        nineBtn.setOnClickListener {
+            solveProblem("9", true)
+        }
+
+        additionBtn.setOnClickListener {
+            solveProblem("+", false)
+        }
+        minusBtn.setOnClickListener {
+            solveProblem("-", false)
+        }
+        multiplyBtn.setOnClickListener {
+            solveProblem("*", false)
+        }
+        divideBtn.setOnClickListener {
+            solveProblem("/", false)
+        }
+        plusOrMinusBtn.setOnClickListener {
+            solveProblem("+/-", false)
+        }
+        percentBtn.setOnClickListener {
+            solveProblem("%", false)
+        }
+
+        clearBtn.setOnClickListener {
+            screenOne.text = ""
+            screenTwo.text = ""
+        }
+        
+//        fun updateDisplay() {
+//            var display = screenOne
+//            var answerDisplay = screenTwo
+//            display.setText(displayValue)
+//            answerDisplay.setText(displayValue)
+//        }
+
+        equalToBtn.setOnClickListener {
+            try {
+                val expression = ExpressionBuilder(screenOne.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if (result == longResult.toDouble()) {
+                    screenTwo.text = longResult.toString()
+                } else {
+                    screenTwo.text = result.toString()
+                }
+            } catch (e: Exception) {
+                Log.d("Exception", " message : " + e.message)
+            }
+        }
+    }
+
+        fun solveProblem(string: String, canClear: Boolean) {
+            if (screenTwo.text.isNotEmpty()) {
+                screenOne.text = ""
+            }
+
+            if (canClear) {
+                screenTwo.text = ""
+                screenOne.append(string)
+            } else {
+                screenOne.append(screenTwo.text)
+                screenOne.append(string)
+                screenTwo.text = ""
+            }
+        }
+
+//        updateDisplay()
+//    }
+}
